3356e483410d54f6566d120c8ae6e4de4f62f1c8
dnhgff <null@dnhgff.space>
2015-04-10 14:32:39 -0700

    update rust head

--- pkgs/development/compilers/rustc/head.nix
+++ pkgs/development/compilers/rustc/head.nix
@@ -3,14 +3,14 @@ callPackage ./makeRustcDerivation.nix {
   shortVersion = "1.0.0-dev";
   isRelease = false;
   # src rev for master on 2015/03/09
-  srcRev = "91bdf23f504f79ed59617cde3dfebd3d5e39a476";
-  srcSha = "1s1v1q630d88idz331n4m0c3ninzipmvfzmkvdaqqm28wsn69xx7";
-  snapshotHashLinux686 = "50a47ef247610fb089d2c4f24e4b641eb0ba4afb";
-  snapshotHashLinux64 = "ccb20709b3c984f960ddde996451be8ce2268d7c";
+  srcRev = "9539627ac76ca37d617a329dbd79c50c59cf59ee";
+  srcSha = "1q0viqgpqnw2yjcmds7y616jysihjgcff17njk40fpgirisbyb9b";
+  snapshotHashLinux686 = "1ef82402ed16f5a6d2f87a9a62eaa83170e249ec";
+  snapshotHashLinux64 = "ef2154372e97a3cb687897d027fd51c8f2c5f349";
   snapshotHashDarwin686 = "ad263bdeadcf9bf1889426e0c1391a7cf277364e";
   snapshotHashDarwin64 = "01c8275828042264206b7acd8e86dc719a2f27aa";
-  snapshotDate = "2015-03-07";
-  snapshotRev = "270a677";
+  snapshotDate = "2015-03-27";
+  snapshotRev = "5520801";
   patches = [
     ./patches/head.patch
   ] ++ stdenv.lib.optional stdenv.needsPax ./patches/grsec.patch;
